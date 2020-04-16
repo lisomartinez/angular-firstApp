92ff2eb10ca4a25b2979457e241fe4a7050e8afa
Lisandro Alejo Martinez <lisandromartinez@gmail.com>
2020-04-15 21:02:10 -0300

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+interface User {
+  name: string,
+  age: number,
+  isSingle: boolean
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +13,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'firstApp';
+  rockbandName: string;
+  rockbands: string[];
+  users: User[];
+
+  constructor() {
+    this.rockbands = ['kiss', 'AC/DC', 'Aerosmith'];
+    this.users = [ {
+      name: 'Lisandro',
+      age: '20',
+      isSingle: true
+    }];
+  }
+
+  addRockBand() {
+    this.rockbands.push(this.rockbandName);
+    this.rockbandName = '';
+  }
+
+  clearRockBands() {
+    this.rockbands = [];
+  }
+
+  deleteItem(index: number) {
+    this.rockbands.splice(index, 1);
+  }
+
+  updateItem(index: number) {
+    this.rockbands[index] = this.rockbandName;
+  }
 }
